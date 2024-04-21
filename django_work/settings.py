631df3d62fb83cabdd0160c612087bdd49bc5068
shaidsifat <42160933+shaidsifat@users.noreply.github.com>
2024-04-21 16:15:16 +0600

    Update settings.py

--- django_work/settings.py
+++ django_work/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-# MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
