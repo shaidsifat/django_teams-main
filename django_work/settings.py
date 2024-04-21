22c0a01f2d80576601f45e4b2929f79f80db8989
shaidsifat <42160933+shaidsifat@users.noreply.github.com>
2024-04-21 16:13:00 +0600

    Update settings.py

--- django_work/settings.py
+++ django_work/settings.py
@@ -80,11 +80,10 @@ WSGI_APPLICATION = 'django_work.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR ,'db.sqlite3'),
     }
 }
 
